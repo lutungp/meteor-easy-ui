5943bffc2806d0bf349d4d5c95b8a6eb1964cbfb
lutungp <lntngp19@gmail.com>
2019-05-15 12:04:11 +0700

    [UPDATE]

--- imports/Methods.jsx
+++ imports/Methods.jsx
@@ -5,26 +5,6 @@ import { render } from 'react-dom';
 class Methods extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            user: {
-                name: null,
-                email: null,
-                hero: null,
-                accept: true
-            },
-            heroes: [
-                { value: 11, text: "Mr. Nice" },
-                { value: 12, text: "Narco" },
-                { value: 13, text: "Bombasto" },
-                { value: 14, text: "Celeritas" },
-                { value: 15, text: "Magneta" },
-                { value: 16, text: "RubberMan" },
-                { value: 17, text: "Dynama" },
-                { value: 18, text: "Dr IQ" },
-                { value: 19, text: "Magma" },
-                { value: 20, text: "Tornado" }
-            ]
-        }
     }
 
     callmethod(params){
